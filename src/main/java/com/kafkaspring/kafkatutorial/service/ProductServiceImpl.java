2b56ab9124777aa3d8dbd6beba12f1f2b9d75680
anandhureji <anandhureji33@gmail.com>
2025-01-15 17:43:49 +0530

    Kafka Producer : Send message asynchronously

--- src/main/java/com/kafkaspring/kafkatutorial/service/ProductServiceImpl.java
+++ src/main/java/com/kafkaspring/kafkatutorial/service/ProductServiceImpl.java
@@ -1,13 +1,46 @@
 package com.kafkaspring.kafkatutorial.service;
 
 import com.kafkaspring.kafkatutorial.controller.CreateProductRestModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductServiceImpl implements ProductService{
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    KafkaTemplate<String,ProductCreatedEvent> kafkaTemplate;
+
+    public ProductServiceImpl(KafkaTemplate<String,ProductCreatedEvent> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+
     @Override
     public String createProduct(CreateProductRestModel createProductRestModel) {
+        String productId = UUID.randomUUID().toString();
 
-        return null;
+        ProductCreatedEvent  productCreatedEvent = new ProductCreatedEvent(productId,
+                createProductRestModel.getTitle(),createProductRestModel.getPrice(),
+                createProductRestModel.getPrice());
+        CompletableFuture<SendResult<String,ProductCreatedEvent>> future =
+        kafkaTemplate.send("new-product-created-events-topic",productId,productCreatedEvent);
+        future.whenComplete((result,exception)->{
+            if(exception!=null){
+                logger.error("Failed to send message",exception.getMessage());
+
+            }else {
+                logger.info("Message sent succesfully",result.getRecordMetadata());
+
+            }
+        });
+
+        return productId;
     }
 }
